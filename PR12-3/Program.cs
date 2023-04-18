7f75174c84af4285ca94c22a11562a3ba08f66d6
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-18 14:54:58 +0300

    [Feature] Add 12rd Practical work on practice

---
+++ PR12-3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR12_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assistant assistant = new Assistant("MAche", "chin", 15, 43);
+            assistant.AddAssistant += Assistant_AddAssistant;
+            assistant.DoSomthing();
+
+            Manager manager = new Manager("Ticho", "novich", 188, 14, 25);
+            manager.AddManager += Manager_AddManager;
+            manager.DoSomthing();
+        }
+
+        private static void Manager_AddManager()
+        {
+            Console.WriteLine("Manager is added");
+        }
+
+        private static void Assistant_AddAssistant()
+        {
+            Console.WriteLine("Assitant is added");
+        }
+    }
+
+    public class Person
+    {
+        public Person(string name, string fIO, int salary)
+        {
+            Name = name;
+            FIO = fIO;
+            Salary = salary;
+        }
+
+        public string Name { get; set; }
+        public string FIO { get; set; }
+        public int Salary { get; set; }
+
+    }
+
+    public class Assistant : Person
+    {
+        public Assistant(string name, string fIO, int salary, int seniority) : base(name, fIO, salary)
+        {
+            Seniority = seniority;
+        }
+
+        public int Seniority { get; set; }
+        public virtual double Salarysum => Salary + Seniority * 0.2;
+
+        public void DoSomthing()
+        {
+            Console.WriteLine("Make something...");
+            AddAssistant?.Invoke();
+        }
+
+        public event Action AddAssistant;
+    }
+
+    public class Manager : Assistant
+    {
+        public Manager(string name, string fIO, int salary, int seniority, int subordinate) : base(name, fIO, salary, seniority)
+        {
+            Subordinate = subordinate;
+        }
+
+        public int Subordinate { get; set; }
+
+        public override double Salarysum => Salary + Subordinate * 0.2 + Seniority * 0.2;
+
+        public event Action AddManager;
+
+        public void DoSomthing()
+        {
+            Console.WriteLine("Make something...");
+            AddManager?.Invoke();
+        }
+    }
+}
---
+++ PR12-4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR12_4
+{
+    public delegate void DelegatSorted(int method);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = {
+            new Book("Книга1", "Автор1", "Издатель1"),
+            new Book("МегаКнига", "МегаАвтор", "МегаИздатель"),
+            new Book("БетаКнига", "БетаАвтор", "БетаИзатель"),
+            new Book("ЙодоКнига", "ЙодаАвтор", "ЙодоИздатель")
+        };
+
+            var bookContainer = new BookContainer(books);
+
+            bookContainer.SortingLibrary(new DelegatSorted(bookContainer.Sort), 1);
+            bookContainer.SortingLibrary(new DelegatSorted(bookContainer.Sort), 2);
+            bookContainer.SortingLibrary(new DelegatSorted(bookContainer.Sort), 3);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public string Publisher { get; }
+
+        public Book(string title, string author, string publisher)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author}, published by {Publisher}";
+        }
+    }
+
+    class BookContainer
+    {
+        private Book[] books;
+
+        public BookContainer(Book[] books)
+        {
+            this.books = books;
+        }
+
+        public void SortingLibrary(DelegatSorted sortDelegate, int i)
+        {
+            sortDelegate.Invoke(i);
+        }
+        public void Sort(int kret)
+        {
+            if (kret == 1)
+            {
+                Console.Write("По заголовку ");
+                books = books.OrderBy(e => e.Title).ToArray();
+            }
+            else if (kret == 2)
+            {
+                Console.Write("По автору ");
+                books = books.OrderBy(e => e.Author).ToArray();
+            }
+            else
+            {
+                Console.Write("По издательству ");
+                books = books.OrderBy(e => e.Publisher).ToArray();
+            }
+            Console.WriteLine("Отсортированныи массив");
+            foreach (var item in books)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void PrintBooks()
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
