0d411b83dd317663938c9e3f815450dcddf8293c
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-28 09:16:42 +0300

    [Feature] Add 20 rd Practical work on practice

---
+++ PR20-2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR20_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+
+            Task[] tasks = new Task[3];
+            tasks[0] = Task.Run(() =>
+            {
+                double x = 3.0;
+                double y = -2.0;
+                double result = Func1(x, y);
+                Console.WriteLine("Результат Func1: " + result);
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                double x = 3.0;
+                double y = 2.0;
+                double result = Func2(x, y);
+                Console.WriteLine("Результат Func2: " + result);
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                double x = 3.0;
+                double y = 0.0;
+                double result = Func3(x, y);
+                Console.WriteLine("Результат Func3: " + result);
+            });
+
+            Task.WaitAll(tasks);
+        }
+
+        static double Func1(double x, double y)
+        {
+            double xy = x * y;
+            if (xy < 0)
+            {
+                return (Math.Log10(Math.Abs(xy)) + 1) / (1 + Math.Pow(y, 2) - x);
+            }
+            throw new NotImplementedException("Error in Func1");
+        }
+
+        static double Func2(double x, double y)
+        {
+            double xy = x * y;
+            if (xy > 0)
+            {
+                return Math.Sqrt(Math.Abs(Math.Sin(x)) + xy);
+            }
+            throw new NotImplementedException("Error in Func2");
+        }
+
+        static double Func3(double x, double y)
+        {
+            double xy = x * y;
+            if (xy == 0)
+            {
+                return xy / (1 - Math.Pow(x, 2) + Math.Pow(y, 3));
+            }
+            throw new NotImplementedException("Error in Func3");
+        }
+    }
+}
---
+++ PR20-4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR20_4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] array = new int[,]
+            {
+            { 11, 20, 31 },
+            { 43, 15, 68 },
+            { 75, 18, 19 }
+            };
+
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            Task<int>[] tasks = new Task<int>[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                int rowIndex = i;
+                tasks[i] = Task.Run(() => CalculateRowSum(array, rowIndex));
+            }
+
+            Task.WaitAll(tasks);
+
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += tasks[i].Result;
+            }
+            double average = (double)sum / (rows * columns);
+
+            Console.WriteLine("Среднее значение: " + average);
+        }
+
+        static int CalculateRowSum(int[,] array, int rowIndex)
+        {
+            int sum = 0;
+            int columns = array.GetLength(1);
+            for (int j = 0; j < columns; j++)
+            {
+                sum += array[rowIndex, j];
+            }
+            Console.WriteLine("Array sum:{0}", sum);
+            return sum;
+        }
+    }
+}
