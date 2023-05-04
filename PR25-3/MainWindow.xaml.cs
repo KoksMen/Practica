c9f3b48093cf2a8ba747f63ba7d8d39412f86d3a
Rin Shima <rinshima@yandex.ru>
2023-05-04 07:33:09 +0300

    [Feature] Add 25rd Practical work on practice

---
+++ PR25-3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR25_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double num1, num2;
+            num1 = Convert.ToDouble(tb1.Text); //x
+            num2 = Convert.ToDouble(tb2.Text); //a
+
+            double result = Math.Pow(Math.E, -num1) * Math.Tan(num2) + Math.Pow(1 / Math.Tan(num1), 2)*Math.Pow((num2*num1), 3);
+            ResultBlock.Text = result.ToString("0.00");   
+        }
+    }
+}
---
+++ PR25-4/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR25_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Text = string.Empty;
+            subspeciesTextBox.Text = string.Empty;
+            lifespanTextBox.Text = string.Empty;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string subspecies = subspeciesTextBox.Text;
+            string lifespan = lifespanTextBox.Text;
+
+            string data = $"{name}, {subspecies}, {lifespan}\n";
+
+            string fileName = "mammals.txt";
+            File.AppendAllText(fileName, data);
+
+            MessageBox.Show("Данные сохранены в файл.");
+        }
+    }
+}
